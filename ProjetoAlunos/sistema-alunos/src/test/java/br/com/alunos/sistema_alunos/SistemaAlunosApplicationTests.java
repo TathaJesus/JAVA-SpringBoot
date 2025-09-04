5da887e6831949c431efc0c26d1324c527496c15
MykyBalb <mykaelbalbino06@gmail.com>
2025-09-03 22:04:21 -0300

    ToDoList Quase Finalizada

---
+++ ProjetoAlunos/sistema-alunos/src/test/java/br/com/alunos/sistema_alunos/SistemaAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alunos.sistema_alunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
